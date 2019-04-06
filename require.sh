1e1cbb11f502f6b822f17ef011cf34217f030c19
Xinyu <xinyu@ifreetion.com>
2019-04-06 14:02:37 +0800

    Remove require.sh

--- require.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-. ./versions.sh
-
-# Preparation
-yum install wget libffi* -y
-yum groupinstall "Development Tools" -y
-yum install centos-release-scl -y
-yum install llvm-toolset-7-clang* devtoolset-7-llvm*
-scl enable devtoolset-7 bash && scl enable llvm-toolset-7 bash
